ded41c891cc48ed66f258582b732d2ddd2d5f6a4
TzeYuon <chantzeyuon@gmail.com>
2026-08-15 20:18:58 +0800

    Update the chatbot to Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# CBT User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/CBT.java
@@ -0,0 +1,22 @@
+public class CBT {
+    /**
+     * Displays a welcome message and a farewell message before exiting.
+     *
+     * @param args command-line arguments, which are not used by this program
+     */
+    public static void main(String[] args) {
+        String banner = "  ____ ____ _____\n"
+                + " / ___| __ )_   _|\n"
+                + "| |   |  _ \\ | |\n"
+                + "| |___| |_) || |\n"
+                + " \\____|____/ |_|\n";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm CBT.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
